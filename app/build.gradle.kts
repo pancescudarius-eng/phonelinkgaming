f57a212b62f715b341369dc9364c69785c528ce1
pancescudarius-eng <pancescudarius@gmail.com>
2026-07-23 04:00:18 +0300

    Bump Cosyra to version 0.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.cosyra.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.5.0"
+        versionCode = 6
+        versionName = "0.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "SIGNALING_URL", "\"$signalingUrl\"")
